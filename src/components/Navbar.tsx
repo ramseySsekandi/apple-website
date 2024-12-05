eb8642e756bdc1efc9c0954d739786a5cacba9ef
ramseySsekandi <moshehravens@gmail.com>
2024-12-05 13:59:32 +0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { navLists } from "../constants"
+import { appleImg, bagImg, blackImg, searchImg } from "../utils"
+
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+        <nav className="flex w-full screen-max-width ">
+          <img src={appleImg} alt="Apple" width={14} height={18} />
+            <div className="flex flex-1 justify-center max-sm:hidden">
+                {
+                navLists.map((nav, i) =>(
+                    <div key={i}>
+                        {nav}
+                    </div>
+                ))
+                }
+            </div>
+            <div>
+                <img src={searchImg} alt="search" width={18} height={18} />
+                <img src={bagImg} alt="bag" width={18} height={18} />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
